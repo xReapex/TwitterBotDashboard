512d35546b08e937a31cae808b9e5fb2ef6834fe
xReapex <tr7296228@gmail.com>
2021-03-05 18:35:19 +0100

    :books: Update server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // env
 require('dotenv').config()
 
+// import path
+const path = require("path");
+
 // express
 const express = require('express')
 const app = express()
@@ -12,15 +15,20 @@ const handlebars = require('express-handlebars')
 app.set('view engine', 'handlebars')
 
 // static
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,"public")));
 
 // layouts 
 app.engine('handlebars', handlebars({
     layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
 }))
  
+// route /
 app.get('/', function (req, res) {
     res.render('main', {layout : 'index'})
  })
  
-app.listen(process.env.EXPRESS_PORT)
+// run server 
+app.listen(process.env.EXPRESS_PORT, function(){
+    console.log('Serveur running on port', process.env.EXPRESS_PORT)
+})
