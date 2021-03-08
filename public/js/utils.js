1aef253c5f9223be1e3d8920937776c8486a6c4a
xReapex <tr7296228@gmail.com>
2021-03-08 21:18:58 +0100

    :lipstick: Code quality

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,4 +1,4 @@
-const { app, open, colors } = require('../../server')
+const { app, open, colors } = require("../../server");
 
 async function launch() {
     //await open(`http://localhost:${process.env.EXPRESS_PORT}`, { wait: true });
@@ -8,7 +8,7 @@ async function launch() {
         console.log("[Server]".cyan, `Server running on port ${process.env.EXPRESS_PORT}`);
         console.log("[Server]".cyan, "Access dashboard :", `http://localhost:${process.env.EXPRESS_PORT}`.grey)
         console.log("[Dashboard]".green, "Dashboard ready to use")
-    })
-}
+    });
+};
 
 exports.launch = launch();
